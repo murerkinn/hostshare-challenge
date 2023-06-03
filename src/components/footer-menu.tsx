2b5e3787b6e99f018cb2f6fc1376016a3458d57f
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-04 01:15:41 +0300

    Create footer menu

---
+++ src/components/footer-menu.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+import { footerMenuItems } from '@/constants/footer-menu-items'
+
+const FooterMenu = () => {
+  return (
+    <div className="flex flex-col py-12 md:flex-row">
+      {footerMenuItems.map(item => (
+        <div key={item.title} className="flex-1 px-3">
+          <h6 className="mb-6 font-semibold">{item.title}</h6>
+
+          <ul className="flex flex-row gap-2 md:flex-col">
+            {item.links.map(link => (
+              <li key={link.title}>
+                <Link
+                  href={link.href}
+                  className="text-sm font-light hover:underline"
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default FooterMenu
---
+++ src/constants/footer-menu-items.ts
@@ -0,0 +1,102 @@
+export const footerMenuItems = [
+  {
+    title: 'Support',
+    links: [
+      {
+        title: 'Help Center',
+        href: '/',
+      },
+      {
+        title: 'HostCover',
+        href: '/',
+      },
+      {
+        title: 'Supporting people with disabilities',
+        href: '/',
+      },
+      {
+        title: 'Cancellation options',
+        href: '/',
+      },
+      {
+        title: 'Our COVID-19 Response',
+        href: '/',
+      },
+      {
+        title: 'Report a neighborhood concern',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: 'Community',
+    links: [
+      {
+        title: '',
+        href: '/',
+      },
+      {
+        title: 'Combating Discrimination',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: 'Hosting',
+    links: [
+      {
+        title: 'Hostshare your home',
+        href: '/',
+      },
+      {
+        title: 'HostCover for hosts',
+        href: '/',
+      },
+      {
+        title: 'Explore hosting resources',
+        href: '/',
+      },
+      {
+        title: 'Visit our community forum',
+        href: '/',
+      },
+      {
+        title: 'How to host responsibly',
+        href: '/',
+      },
+      {
+        title: 'Hostshare-friendly apartments',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: 'Hostshare',
+    links: [
+      {
+        title: 'Newsroom',
+        href: '/',
+      },
+      {
+        title: 'Learn about new features',
+        href: '/',
+      },
+      {
+        title: 'Letter from our founders',
+        href: '/',
+      },
+      {
+        title: 'Careers',
+        href: '/',
+      },
+      {
+        title: 'Investors',
+        href: '/',
+      },
+      {
+        title: 'Gift cards',
+        href: '/',
+      },
+    ],
+  },
+]
