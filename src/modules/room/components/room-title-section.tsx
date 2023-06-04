fb7fc72507383104feed11113c6a8bdb596fa542
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-04 21:43:40 +0300

    Create room title section

---
+++ src/modules/room/components/room-title-section.tsx
@@ -0,0 +1,53 @@
+import { Location } from '../types'
+
+type RoomTitleSectionProps = {
+  title: string
+  visibleReviewCount: number
+  location: Location
+}
+
+const RoomTitleSection = ({
+  title,
+  visibleReviewCount,
+  location,
+}: RoomTitleSectionProps) => {
+  return (
+    <section className="py-6">
+      <h1 className="mb-1 text-2xl font-semibold">{title}</h1>
+
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row items-center">
+          {visibleReviewCount > 0 && (
+            <>
+              <button className="btn btn-link p-0">
+                <span>
+                  {visibleReviewCount}{' '}
+                  {visibleReviewCount === 1 ? 'review' : 'reviews'}
+                </span>
+              </button>
+              <span className="mx-2">·</span>
+            </>
+          )}
+
+          <button className="btn btn-link p-0">
+            <span>
+              {location.city}, {location.country.title}
+            </span>
+          </button>
+        </div>
+
+        <div className="flex flex-row gap-5">
+          <button className="btn btn-link">
+            <span>Share</span>
+          </button>
+
+          <button className="btn btn-link">
+            <span>Save</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default RoomTitleSection
