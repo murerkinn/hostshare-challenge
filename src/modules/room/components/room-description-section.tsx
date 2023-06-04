fed031be2cdadb7f297ce70ce6d0364602862037
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-05 00:47:04 +0300

    Create room description section

---
+++ src/modules/room/components/room-description-section.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/no-children-prop */
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+type RoomDescriptionSectionProps = {
+  description: string
+}
+
+const RoomDescriptionSection = ({
+  description,
+}: RoomDescriptionSectionProps) => {
+  const isOver300Chars = description.length > 300
+
+  return (
+    <section className="pb-12 pt-8 border-b border-b-border-light-gray">
+      <ReactMarkdown
+        components={{
+          p: ({ node, ...props }) => (
+            <p className="text-base leading-6" {...props} />
+          ),
+        }}
+        children={
+          isOver300Chars ? `${description.slice(0, 300)}...` : description
+        }
+        remarkPlugins={[remarkGfm]}
+      />
+
+      {isOver300Chars && (
+        <button className="btn btn-link btn-lg pl-0 mt-4">
+          <span>Show more</span>
+        </button>
+      )}
+    </section>
+  )
+}
+
+export default RoomDescriptionSection
