4afd528323a8cf97f7dde1f746e9c385e8eb4956
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-04 21:43:59 +0300

    Add type definitions for the room module

---
+++ src/modules/room/types.ts
@@ -0,0 +1,85 @@
+type Currency = {
+  code: string
+  name: string
+  symbol: string
+}
+
+type Country = {
+  code: string
+  title: string
+}
+
+export type Location = {
+  address: string
+  city: string
+  zip: string
+  country: Country
+  lat: number
+  long: number
+}
+
+export type Image = {
+  aspectRatio: number
+  height: number
+  mimeType: string
+  orientation: string
+  type: string
+  url: string
+  width: number
+}
+
+export type Host = {
+  avatar: Image
+  isSuperHost: boolean
+  name: string
+}
+
+type Ratings = {
+  accuracy: number
+  checkin: number
+  cleanliness: number
+  communication: number
+  guestSatisfactionOverall: number
+  location: number
+  value: number
+}
+
+export type Amenity = {
+  available: boolean
+  group: string
+  title: string
+  type: string
+}
+
+export type ListingAmenities = {
+  count: number
+  data: Amenity[]
+}
+
+export type ListingImages = {
+  count: number
+  data: Image[]
+}
+
+type ListingInfo = {
+  id: string
+  available: boolean
+  title: string
+  description: string
+  type: string
+  maxGuestCapacity: number
+  price: number
+  currency: Currency
+  location: Location
+  mainImage: Image
+  images: ListingImages
+  amenities: ListingAmenities
+  host: Host
+  ratings: Ratings
+  visibleReviewCount: number
+}
+
+export type Listing = {
+  info: ListingInfo
+  ref: string
+}
