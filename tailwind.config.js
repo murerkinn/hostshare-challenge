b30f56ae8f8dac587cfe3cb62da0cc731be31579
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-04 23:23:00 +0300

    Add button styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#329a9a',
+        'border-gray': 'rgb(221, 221, 221)',
+        'border-mid-gray': 'rgb(235, 235, 235)',
+        'border-light-gray': 'rgba(0, 0, 0, 0.08)',
+        'text-gray': 'rgb(34, 34, 34)',
+        'btn-hover': 'rgb(247, 247, 247)',
+        'mid-gray': 'rgb(113, 113, 113)',
+      },
+      fontSize: {
+        xxs: ['10px', '12px'],
+        base: ['16px', '20px'],
+        'section-title': ['22px', '26px'],
+      },
+    },
   },
   plugins: [require('@headlessui/tailwindcss')],
 }
