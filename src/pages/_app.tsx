e37fc1392fd8327d5b3cfda9c14131e2813a3693
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-05 10:44:23 +0300

    Remove unnecessary axios request in the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import '@/styles/main.scss'
 
-import axios from 'axios'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    axios.get('/api/search').then(res => console.log(res.data))
-  }, [])
-
   return <Component {...pageProps} />
 }
 
