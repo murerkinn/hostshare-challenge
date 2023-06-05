a9d41ba44e51c851b4a154c8dd7299875e0bfb7a
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-05 23:18:42 +0300

    Fix build

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,10 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import listingsData from '@/data/listings.json'
+import rawData from '@/data/listings.json'
+import { Listing } from '@/modules/room/types'
 
-type ResponseData = {
+type ListingsData = {
+  data: Listing[]
+  count: number
+}
+
+type SuccessResponseData = {
+  listings: Listing[]
+  count: number
+}
+
+type ErrorResponseData = {
   message: string
 }
+
+type ResponseData = SuccessResponseData | ErrorResponseData
 
 export default function handler(
   req: NextApiRequest,
@@ -13,6 +26,8 @@ export default function handler(
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' })
   }
+
+  const listingsData = rawData as unknown as ListingsData
 
   const page = Number(req.query.page) || 1
   const limit = Number(req.query.limit) || 20
