c37ab1e4100bffee760ddf3cf7ee8ec78076bc86
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-05 10:43:27 +0300

    Adjust footer menu styles

--- src/components/footer-menu.tsx
+++ src/components/footer-menu.tsx
@@ -7,15 +7,12 @@ const FooterMenu = () => {
     <div className="flex flex-col py-12 md:flex-row">
       {footerMenuItems.map(item => (
         <div key={item.title} className="flex-1 px-3">
-          <h6 className="mb-6 font-semibold">{item.title}</h6>
+          <h6 className="mb-6 font-medium">{item.title}</h6>
 
-          <ul className="flex flex-row gap-2 md:flex-col">
+          <ul className="flex flex-row gap-4 md:flex-col">
             {item.links.map(link => (
               <li key={link.title}>
-                <Link
-                  href={link.href}
-                  className="text-sm font-light hover:underline"
-                >
+                <Link href={link.href} className="text-sm hover:underline">
                   {link.title}
                 </Link>
               </li>
