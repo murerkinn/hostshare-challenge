a146647c9e6192bfa2438c3564d5c14171c2c99c
Murat Erkin Çiçek <murerkinn@gmail.com>
2023-06-05 13:52:37 +0300

    Allow remote image urls in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a0.muscache.com',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
